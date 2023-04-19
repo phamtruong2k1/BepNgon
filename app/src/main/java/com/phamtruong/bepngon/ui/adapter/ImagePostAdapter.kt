d2e2f381f7bd07f092d6d55bbbf8c9a4e53c9edb
Phạm Ánh Trường <61191253+phamtruong2k1@users.noreply.github.com>
2023-04-19 17:20:48 +0700

    create ui post

--- app/src/main/java/com/phamtruong/bepngon/ui/adapter/ImagePostAdapter.kt
+++ app/src/main/java/com/phamtruong/bepngon/ui/adapter/ImagePostAdapter.kt
@@ -3,54 +3,55 @@ package com.phamtruong.bepngon.ui.adapter
 import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.phamtruong.bepngon.R
 import com.phamtruong.bepngon.model.RecipeModel
 
-class ImageAdapter(
+class ImagePostAdapter(
     var context: Context,
-    private var listData: List<Uri>,
+    private var listData: ArrayList<Uri>,
     val listener: EventClickImageAdapterListener
-) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<ImagePostAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-//        var img: ImageView = view.findViewById(R.id.imgEijoy)
-//        var name : TextView = view.findViewById(R.id.txtEijoy)
+        var imgImage: ImageView = view.findViewById(R.id.imgImage)
+        var imgDelete: ImageView = view.findViewById(R.id.imgDelete)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.item_bai_viet, viewGroup, false)
+            .inflate(R.layout.item_image_post, viewGroup, false)
 
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-//        val recipe = listData[position]
-//        viewHolder.name.text = context.getString(eijoy.name)
-//
-//        viewHolder.img.setImageDrawable(ResourcesCompat.getDrawable(context.resources, eijoy.img, null))
-//
-//        viewHolder.img.setOnTouchScale( {
-//            PlaySound.soundClick(context)
-//            listener.clickEijoyTab(eijoy.id)
-//        }, 0.9f, false)
+        val image = listData[position]
+
+        viewHolder.imgImage.setImageBitmap(MediaStore.Images.Media.getBitmap(context.contentResolver, image))
+
+        viewHolder.imgDelete.setOnClickListener {
+
+            listener.click(position)
+        }
     }
 
 
     @SuppressLint("NotifyDataSetChanged")
-    fun setListData(arr : List<Uri>) {
+    fun setListData(arr : ArrayList<Uri>) {
         listData = arr
         notifyDataSetChanged()
     }
 
-    override fun getItemCount() = 9
+    override fun getItemCount() = listData.size
 
 }
 
 interface EventClickImageAdapterListener {
-    fun click(idTab : Int)
+    fun click(pos : Int)
 }
--- app/src/main/java/com/phamtruong/bepngon/ui/baidang/DangBaiActivity.kt
+++ app/src/main/java/com/phamtruong/bepngon/ui/baidang/DangBaiActivity.kt
@@ -1,5 +1,6 @@
 package com.phamtruong.bepngon.ui.baidang
 
+import android.annotation.SuppressLint
 import android.app.ProgressDialog
 import android.content.Intent
 import android.net.Uri
@@ -12,19 +13,28 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.phamtruong.bepngon.databinding.ActivityDangBaiBinding
+import com.phamtruong.bepngon.ui.adapter.EventClickImageAdapterListener
+import com.phamtruong.bepngon.ui.adapter.ImagePostAdapter
 import com.phamtruong.bepngon.util.Constant.TAG
+import com.phamtruong.bepngon.view.gone
+import com.phamtruong.bepngon.view.show
 import java.io.IOException
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class DangBaiActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityDangBaiBinding
 
+    lateinit var adapter: ImagePostAdapter
+
     private var filePath: Uri? = null
 
     var storage: FirebaseStorage? = null
     var storageReference: StorageReference? = null
+
+    var listImage = ArrayList<Uri>()
 
     companion object {
         const val PICK_IMAGE_REQUEST = 12345
@@ -34,6 +44,19 @@ class DangBaiActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDangBaiBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        adapter = ImagePostAdapter(this, listImage, object : EventClickImageAdapterListener{
+            @SuppressLint("NotifyDataSetChanged")
+            override fun click(pos: Int) {
+                listImage.removeAt(pos)
+                if (listImage.size == 0) {
+                    binding.llImage.gone()
+                }
+                adapter.notifyDataSetChanged()
+            }
+        })
+
+        binding.rcyImage.adapter = adapter
 
         storage = FirebaseStorage.getInstance()
         storageReference = storage?.reference
@@ -60,13 +83,17 @@ class DangBaiActivity : AppCompatActivity() {
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
             filePath = data.data
             try {
-                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
-                binding.imageView.setImageBitmap(bitmap)
+                filePath?.let {
+                    listImage.add(it)
+                    adapter.notifyDataSetChanged()
+                    binding.llImage.show()
+                }
             } catch (e: IOException) {
                 e.printStackTrace()
             }
@@ -74,34 +101,45 @@ class DangBaiActivity : AppCompatActivity() {
     }
 
     private fun uploadImage() {
-        if (filePath != null) {
-            val progressDialog = ProgressDialog(this)
-            progressDialog.setTitle("Uploading...")
-            progressDialog.show()
-            val ref = storageReference!!.child("images/" + UUID.randomUUID().toString())
-            ref.putFile(filePath!!)
-                .addOnSuccessListener {
-                    progressDialog.dismiss();
-                    Toast.makeText(this, "Uploaded", Toast.LENGTH_SHORT).show();
-                    val downloadUri: Task<Uri> = it.storage.downloadUrl
-                    downloadUri.addOnSuccessListener {
-                        var imageLink = it.toString()
-                        Log.d(TAG, "uploadImage: $imageLink")
-                    }.addOnFailureListener {
-                        Log.d(TAG, "uploadImage: fail")
-                    }
+        if (listImage.size == 0) {
+            upPost()
+        } else {
+            listImage.let { dataImage ->
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Uploading...")
+                progressDialog.show()
+                dataImage.forEachIndexed { index, image ->
+                    val ref = storageReference!!.child("images/" + UUID.randomUUID().toString())
+                    ref.putFile(image)
+                        .addOnSuccessListener {
+                            progressDialog.dismiss();
+                            Toast.makeText(this, "Uploaded", Toast.LENGTH_SHORT).show();
+                            val downloadUri: Task<Uri> = it.storage.downloadUrl
+                            downloadUri.addOnSuccessListener { link ->
+                                val imageLink = link.toString()
+                                Log.d(TAG, "uploadImage: $imageLink")
+                            }.addOnFailureListener {
+                                Log.d(TAG, "uploadImage: fail")
+                            }
+                        }
+                        .addOnFailureListener { e ->
+                            progressDialog.dismiss()
+                            Toast.makeText(this, "Failed " + e.message, Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        .addOnProgressListener { taskSnapshot ->
+                            val progress =(index *(100.0/dataImage.size)) + 100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount / dataImage.size
+                            progressDialog.setMessage("Uploaded " + progress.toInt() + "%")
+                        }
                 }
-                .addOnFailureListener { e ->
-                    progressDialog.dismiss()
-                    Toast.makeText(this, "Failed " + e.message, Toast.LENGTH_SHORT)
-                        .show()
-                }
-                .addOnProgressListener { taskSnapshot ->
-                    val progress = 100.0 * taskSnapshot.bytesTransferred / taskSnapshot
-                        .totalByteCount
-                    progressDialog.setMessage("Uploaded " + progress.toInt() + "%")
-                }
+                upPost()
+            }
         }
+    }
+
+    private fun upPost() {
+        Toast.makeText(this, "Post...", Toast.LENGTH_SHORT)
+        Log.d(TAG, "uploadImage: Post....")
     }
 
     override fun onBackPressed() {
